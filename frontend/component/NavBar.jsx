8892c01776a4ea7cff59b1e9700b58e7c2af143c
Balinder (sultan) <144314728+Sultanpal@users.noreply.github.com>
2024-11-09 10:53:38 +0530

    Add files via upload

---
+++ frontend/component/NavBar.jsx
@@ -0,0 +1,11 @@
+export function NavBar1(){
+    return (
+        <div>
+            <div>Payment APP</div>
+            <div>
+                <div>hello {User.name}</div>
+                <div>U</div>
+            </div>
+        </div>
+    )
+}
